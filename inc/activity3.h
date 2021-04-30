642a13623eb56845476d14035b75cd9f082c8679
ragasrikonakalla <ragasrikonakalla@gmail.com>
2021-04-30 15:02:14 +0530

    commit

--- inc/activity3.h
+++ inc/activity3.h
@@ -1,9 +1,24 @@
 #ifndef _ACTIVITY_3_
 #define _ACTIVITY_3_
-
+/**
+ * @file activity3.h
+ * @author Ragasri K
+ * @brief To generate pwm signal 
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
+/**
+ * @brief PWM generation with prescaler 64. 
+ * 
+ * @param temp
+ * @return char
+ */
 
-void PWM(uint16_t temp);
 void initPWM(void);
+char PWM(uint16_t temp);
 
 #endif
